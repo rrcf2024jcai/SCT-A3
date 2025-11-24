8827b69d7184aa6b9e97abe363477697728d07d4
rrcf2024jcai <jcai2@academic.rrc.ca>
2025-11-24 12:32:29 -0600

    Touch vulnerable lines to trigger scan

--- assignment1_code_sample.py
+++ assignment1_code_sample.py
@@ -16,12 +16,12 @@ def send_email(to, subject, body):
     os.system(f'echo {body} | mail -s "{subject}" {to}')
 
 def get_data():
-    url = 'http://insecure-api.com/get-data'
+    url = 'http://insecure-api.com/get-data '
     data = urlopen(url).read().decode()
     return data
 
 def save_to_db(data):
-    query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
+    query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')  "  
     connection = pymysql.connect(**db_config)
     cursor = connection.cursor()
     cursor.execute(query)
